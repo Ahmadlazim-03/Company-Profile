9b46fa2a6ae6a36e4a1f341f901e2895b73879ba
yangTauTauAjah <tesakun6464@gmail.com>
2025-04-02 12:42:48 +0700

    define new routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,52 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\ContentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Rute untuk notifikasi
+Route::get('/notifications', [NotificationController::class, 'index']);
+Route::get('/notifications/{id}', [NotificationController::class, 'show']);
+Route::middleware('auth:api')->group(function () {
+    Route::post('/notifications', [NotificationController::class, 'store']);
+    Route::put('/notifications/{id}', [NotificationController::class, 'update']);
+    Route::delete('/notifications/{id}', [NotificationController::class, 'destroy']);
+});
+
+// Rute untuk konten
+Route::get('/contents', [ContentController::class, 'index']);
+Route::get('/contents/{id}', [ContentController::class, 'show']);
+Route::middleware('auth:api')->group(function () {
+    Route::post('/contents', [ContentController::class, 'store']);
+    Route::put('/contents/{id}', [ContentController::class, 'update']);
+    Route::delete('/contents/{id}', [ContentController::class, 'destroy']);
+});
+
+// Rute untuk mahasiswa
+Route::get('/students', [StudentController::class, 'index']);
+Route::get('/students/{id}', [StudentController::class, 'show']);
+Route::middleware('auth:api')->group(function () {
+    Route::post('/students', [StudentController::class, 'store']);
+    Route::put('/students/{id}', [StudentController::class, 'update']);
+    Route::delete('/students/{id}', [StudentController::class, 'destroy']);
+});
+
+// Rute untuk admin
+Route::get('/admins', [AdminController::class, 'index']);
+Route::get('/admins/{id}', [AdminController::class, 'show']);
+Route::middleware('auth:api')->group(function () {
+    Route::post('/admins', [AdminController::class, 'store']);
+    Route::put('/admins/{id}', [AdminController::class, 'update']);
+    Route::delete('/admins/{id}', [AdminController::class, 'destroy']);
+});
+
+// Rute untuk pengguna biasa
+Route::get('/regular-users', [UserController::class, 'index']);
+Route::get('/regular-users/{id}', [UserController::class, 'show']);
