c72d694ae36ec647e22aba79c18da54276ee7d78
Ahmad Lazim <ahmadlazim422@gmail.com>
2025-04-09 03:55:14 +0700

    Create Middleware, Product & Notification Table ( With API )

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use App\Http\Middleware\IsLogin;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\NotificationController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(IsLogin::class)->group(function () {
+    // LIST ROUTE BUAT HALAMAN YANG PERLU LOGIN TERLEBIH DAHULU
+    // TARUH DI SINI YAAA
+});
+
+// LIST API 
+// KALO MAU AMBIL DATA TINGGAL FETCH LEWAT ROUTE INI 
+Route::get('/api/notification', [NotificationController::class, 'index'])->name('notification.index');
+Route::get('/api/product', [ProductController::class, 'index'])->name('product.index');
+
+
+
+
